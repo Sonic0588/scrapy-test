419b308c27545e5effcda1770690f2eaae82a967
Evgeniy Orlov <sonic050288@gmail.com>
2019-08-09 17:56:42 +0300

    Add parse date

--- aceee/aceee/spiders/aceee_spider.py
+++ aceee/aceee/spiders/aceee_spider.py
@@ -7,20 +7,19 @@ import time
 class AceeeSpider(scrapy.Spider):
     name = 'aceee'
     start_urls = ['https://aceee.org/news-blog', ]
-    #posts_urls = set()
 
     def parse(self, response):
 
         for post in response.xpath('''//div[@class="news-info-container clearfix"]
                                        /div[@class="news-content-box"]/h2/a/@href''').extract():
 
-            #self.posts_urls.add('https://aceee.org' + post)
             yield response.follow('https://aceee.org' + post, callback = self.parse_post)
         
         next_page = response.xpath('//li[@class="next"]/a/@href').get()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
         
+
     def parse_post(self, response):
         item = AceeeItem()
 
@@ -30,24 +29,28 @@ class AceeeSpider(scrapy.Spider):
         PUBDATE = response.xpath('//div[@class="views-field views-field-created"]/span[@class="field-content"]/text()').get()
 
         if '|' in PUBDATE:
-            date_str = PUBDATE[3:-10]
-            print(date_str)
-            date_obj = time.strptime(date_str, "%B %d, %Y")
-        else:
-            date_obj = time.strptime(PUBDATE, "%B %d, %Y")
+            date_list = PUBDATE.split()
+            PUBDATE = ' '.join(date_list[1:4])
 
+        date_obj = time.strptime(PUBDATE, "%B %d, %Y")
         item['PUBDATE'] = time.strftime("%Y-%m-%d", date_obj)
 
         # CATEGORIES = response.xpath('//div[contains(@class, "col-sm-9")]/p/text()').extract()
         # item['CATEGORIES'] = CATEGORIES or []
 
-        ARTICLE_TEXT = ' '.join(response.xpath('//div[@class="new_content_body"]//text()').extract())
-        item['ARTICLE_TEXT'] = ARTICLE_TEXT
+        # ARTICLE_TEXT = ' '.join(response.xpath('//div[@class="new_content_body"]//text()').extract())
+        # item['ARTICLE_TEXT'] = ARTICLE_TEXT
 
-        TAGS = list(response.xpath('//div[@class="views-field views-field-term-node-tid"]/span[@class="field-content"]//text()').extract())
-        item['TAGS'] = TAGS or []
+        TAGS = response.xpath('//div[@class="views-field views-field-term-node-tid"]/span[@class="field-content"]//text()').extract()
+        if TAGS:
+            result = []
+            for el in TAGS:
+                if ',' not in el:
+                    result.append(el)
+            TAGS = result
+        item['TAGS'] = TAGS
 
-        # HYPERLINKS = response.xpath('//div[contains(@class, "col-sm-9")]/p/text()').extract()
-        # item['HYPERLINKS'] = HYPERLINKS or []
+        HYPERLINKS = response.xpath('//div[@class="new_content_body"]//a/@href').extract()
+        item['HYPERLINKS'] = HYPERLINKS
         
         yield item
